9094d4d4632fc4261f5192ea73bdd630a8784068
Cj-bc <cj-bc@y-modify.org>
2016-02-15 20:55:24 +0900

    The Setting Part Finish

--- calculatepixel.c
+++ calculatepixel.c
@@ -13,23 +13,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//=====================Main=====================================================
 
 int main(void)
 {
+//============================== Set_Up ==========================================
+
  //Set Variables---------------------
 	int decimal[257];
-	int hexadecimal[2][257];
+	int hexadecimal[257][2];
 
+ //Set first_num---------------------
 	int place = 0; //Changing point
 	while(place <= 257)
 	{
 		for(i = 0;i <= 15;i ++)
 		{
-			hexadecimal[1][place] = (char)i;
+			hexadecimal[place][1] = i;
 			place ++;
 		}
 	}
+
+ //Set Second_num---------------------
+	int secondplace = 0;
+        for(i = 0;i <= 15;i ++)
+        {
+        	secondplace ++;
+        }
+
+	for(i = 1;i <= 15;i ++)
+	{
+		for(j = 0;j <= 15;j ++)
+		{
+			hexadecimal[secondplace][0] = i;
+			secondplace ++;
+		}
+	}
+
+ //====================== Main ===================================================
+
+	
 
 }
 
